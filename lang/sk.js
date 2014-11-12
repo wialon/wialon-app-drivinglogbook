a88c0ae039d9598bdbfff585d2a886fcfb528b2e
GPS Portal <gps@gpsportal.sk>
2014-11-12 16:13:29 +0100

    v1.5

---
+++ lang/sk.js
@@ -0,0 +1,125 @@
+TRANSLATIONS = {
+	
+	"Driving Logbook": "Kniha jázd",
+
+	"Logbook": "Kniha jázd",
+
+	"Unit": "Vozidlo",
+	
+	"From": "Od",
+	
+	"To": "Do",
+	
+	"OK": "OK",
+	
+	"Beginning": "Začiatok",
+	
+	"End": "Koniec",
+	
+	"Duration": "Trvanie",
+	
+	"Initial location": "Počiatočná adresa",
+	
+	"Final location": "Konečná adresa",
+
+	"Initial mileage": "Počiatočná vzdialenosť",
+
+	"Final mileage": "Konečná vzdialenosť",
+
+	"Mileage": "Ubehnuté km",
+	
+	"Driver": "Vodič",
+	
+	"User": "Užívateľ",
+	
+	"Last changes": "Posledná zmena",
+	
+	"Trip status": "Jazda",
+	
+	"Business": "Služobná",
+	
+	"Personal": "Súkromná",
+	
+	"Notes": "Poznámky",
+	
+	"Page&nbsp;": "Strana&nbsp;",
+	
+	"&nbsp;of&nbsp;": "&nbsp;od&nbsp;",
+	
+	"Apply": "Uložiť",
+	
+	"Displaying %d to %d of %d items": "Zobraziť %d do %d od %d záznamy",
+	
+	"List of units empty.": "Zoznam vozidiel je prázdny",
+	
+	"Login error.": "Chyba prihlásenia",
+	
+	"Please select unit.": "Prosím, vyberte vozidlo",
+	
+	"Please select time interval.": "Prosím, vyberte časový interval",
+	
+	"No data for selected interval.": "Žiadne dáta pre zvolené obdobie",
+
+    "km": "Km",
+
+    "mi": "Mi",
+
+    "%d pages_1": "%d Strana",
+    "%d pages_2": "%d Strán",
+    "%d pages_5": "%d Strán",
+
+    "Start": "Štart",
+
+    "Prev": "Nasled.",
+
+    "Next": "Predch.",
+
+    "Print": "Tlačiť",
+
+    "Yesterday": "Včera",
+    "Today": "Dnes",
+    "Week": "Týždeň",
+    "Month": "Mesiac",
+    "Custom": "Dátum",
+
+    "Sunday": "Nedeľa",
+    "Monday": "Pondelok",
+    "Tuesday": "Utorok",
+    "Wednesday": "Streda",
+    "Thursday": "Štvrtok",
+    "Friday": "Piatok",
+    "Saturday": "Sobota",
+
+    "January": "Január",
+    "February": "Február",
+    "March": "Marec",
+    "April": "Apríl",
+    "May": "Máj",
+    "June": "Jún",
+    "July": "Júl",
+    "August": "August",
+    "September": "September",
+    "October": "Október",
+    "November": "November",
+    "December": "December",
+
+    "Sun": "Ne",
+    "Mon": "Po",
+    "Tue": "Ut",
+    "Wed": "St",
+    "Thu": "Št",
+    "Fri": "Pi",
+    "Sat": "So",
+
+    "Jan": "Jan",
+    "Feb": "Feb",
+    "Mar": "Mar",
+    "Apr": "Apr",
+    "Jun": "Jún",
+    "Jul": "Júl",
+    "Aug": "Aug",
+    "Sep": "Sep",
+    "Oct": "Okt",
+    "Nov": "Nov",
+    "Dec": "Dec"
+}
